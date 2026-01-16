2906d85bd1bf0811ed58ea4ffc7db3bfe313ebab
lcy-mich <michaelcyluk@outlook.com>
2026-01-16 14:36:02 +0000

    fix

--- Exercises/CoinChange.py
+++ Exercises/CoinChange.py
@@ -1,7 +1,6 @@
 def mincoin(coinvalues, target):
-    coinvalues = sorted(coinvalues, reverse=True)
     additions = []
-    for value in coinvalues:
+    for value in sorted(coinvalues, reverse=True):
         mult = 1
         if value <= target:
             while value*mult <= target:
@@ -12,4 +11,11 @@ def mincoin(coinvalues, target):
             target -= value
     return additions
 
-print(mincoin([1,2,5,10,20,50], 28))
+
+assert(mincoin([1,2,5,10,20,50], 7) == [5,2])
+assert(mincoin([1,2,5,10,20,50], 150) == [50,50,50])
+assert(mincoin([1,2,5,10,20,50], 28) == [20,5,2,1])
+assert(mincoin([1,4,6], 7) == [6,1])
+print(mincoin([1,4,6], 9)) #doesn't work
+
+
